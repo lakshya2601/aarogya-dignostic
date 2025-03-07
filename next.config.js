d0f0f2e0f76aa1e3ef1a9572d46714f3a20804c4
Lakshya Porwal <89459982+lakshya2601@users.noreply.github.com>
2025-03-07 19:54:37 +0530

    built errors

--- next.config.js
+++ next.config.js
@@ -1,20 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    formats: ['image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
-  experimental: {
-    optimizeCss: true,
-    scrollRestoration: true,
-  },
   webpack: (config, { dev, isServer }) => {
-    // Optimize bundle size
     if (!dev && !isServer) {
       config.optimization.splitChunks = {
         chunks: 'all',
@@ -25,7 +22,7 @@ const nextConfig = {
         maxInitialRequests: 30,
         cacheGroups: {
           defaultVendors: {
-            test: /[\\/]node_modules[\\/]/,
+            test: /[\/]node_modules[\/]/,
             priority: -10,
             reuseExistingChunk: true,
           },
